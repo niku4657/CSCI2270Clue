68e37245535b639b1fbfbbc92d7eedb162ad72c6
Tanvi Gopalabhatla <tago3904@colorado.edu>
2019-04-15 22:09:04 -0600

    Changed

---
+++ Player.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Player.hpp"
+#include "Human.hpp"
+#include "Computer.hpp"
+
+using namespace std;
+
+
+Player::Player(){
+
+}
+
+void Player::Turn(){
+
+}
+
+int Player::Dice(){
+
+}
+
+void Human::Move(){
+
+}
