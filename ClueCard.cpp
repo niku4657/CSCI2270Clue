558ec6de909001ec3afc22073edb941c946407fc
namitapasupuleti <49174841+namitapasupuleti@users.noreply.github.com>
2019-04-15 22:22:37 -0600

    faas

---
+++ ClueCard.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void ClueCard::shuffleCards(){
+
+}
+
+void ClueCard::combineCards(){
+
+}
+
+void ClueCard::distrubuteCards(){
+
+}
+
+string ClueCard::getCardType(){
+  
+}
+
+string ClueCard::getCardName(){
+
+}
+
+string ClueCard::revealCard(){
+
+}
