fc0265e96373841a3e09ec8c6981f42dadb55138
Tanvi Gopalabhatla <tago3904@colorado.edu>
2019-04-15 21:57:46 -0600

    All the cpps for the player stuff.

---
+++ Computer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Computer.hpp"
+
+using namespace std;
+
+Computer::Computer(){
+
+
+}
+
+void Computer::RandomTurn(){
+
+
+}
+
+void Computer::RandomMove(){
+
+
+}
+
+void Computer::RandomSuggest(bool final){
+
+}
+
+void Computer::FinalAccusation(){
+
+
+}
---
+++ Human.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Human.hpp"
+
+using namespace std;
+
+Human::Human(){
+
+}
+
+void Human::AcceptTurn(){
+
+
+}
+
+void Human::Suggest(bool final){
+
+
+
+}
+
+void Human::ChooseMove(){
+
+
+}
+
+void Human::FinalAccusation(){
+
+
+}
