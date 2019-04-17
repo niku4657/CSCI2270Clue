3c8500b65191f29f53ab704cef8a726d087790e3
namitapasupuleti <49174841+namitapasupuleti@users.noreply.github.com>
2019-04-16 21:36:31 -0600

    working clue.cpp

--- ClueCard.cpp
+++ ClueCard.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include "HashTable.hpp"
+#include "ClueCard.hpp"
 
 using namespace std;
 
+void ClueCard::addCardArray(Card cards[], int _key, string _name){
+  cards[_key] = {_key, _name};
+}
+
+void ClueCard::fillInWeapons(){
+  addCardArray(weapon, 0, "C4C Food");
+  addCardArray(weapon, 1, "Ralphie's Horn");
+  addCardArray(weapon, 2, "CSCI 2270 Midterm 2");
+  addCardArray(weapon, 3, "Dean's List");
+  addCardArray(weapon, 4, "Flu from the Dorms");
+  addCardArray(weapon, 5, "Freshmen Ego");
+
+}
+
+void ClueCard::fillInSuspects(){
+  addCardArray(suspect, 6, "Ralphie");
+  addCardArray(suspect, 7, "Chip the Buffalo");
+  addCardArray(suspect, 8, "Mr. Gold");
+  addCardArray(suspect, 9, "Ms. Black");
+  addCardArray(suspect, 10, "Prashil");
+  addCardArray(suspect, 11, "Tanvi");
+
+}
+
+void ClueCard::fillInRooms(){
+  addCardArray(room, 12, "C4C Food");
+  addCardArray(room, 13, "Ralphie's Horn");
+  addCardArray(room, 14, "CSCI 2270 Midterm 2");
+  addCardArray(room, 15, "Dean's List");
+  addCardArray(room, 16, "Flu from the Dorms");
+  addCardArray(room, 17, "Freshmen Ego");
+
+}
+
+void ClueCard::printArray(){
+    for (int i = 0; i < 6; i++){
+        cout << weapon[i].key << ":" << weapon[i].name << endl;
+    }
+}
+
 void ClueCard::shuffleCards(){
+
   // traverse through main hash table
   // add each element to a new array containing all cards
   // shuffle cards in array
@@ -32,8 +75,13 @@ string ClueCard::getCardName(){
 
 }
 
-string ClueCard::revealCard()
-{
+string ClueCard::revealCard(){
   // choose card to show card to show player
   // insert card into player's hashtable
 }
+
+// int main(){
+//     ClueCard cards;
+//     cards.fillInWeapons();
+//     cards.printArray();
+// }
