391ae0a433d5c3ac74cdd38c86b4fe1901b39547
Tanvi Gopalabhatla <tago3904@colorado.edu>
2019-04-16 18:45:00 -0600

    Merge pull request #6 from niku4657/MENUS
    SJHLIRSGRSGEIGYRUG

--- Player.cpp
+++ Player.cpp
@@ -8,17 +8,24 @@ using namespace std;
 
 Player::Player()
 {
+  //Empty.
+}
+
+void Player::Turn()
+{
+  cout << "Main Menu" << endl;
+  cout << "1. Roll" << endl;
+  cout << "2. Suggest" << endl;
+  cout << "3. Final Accusation" << endl;
+}
+
+int Player::Dice()
+{
 
 }
 
-void Player::Turn(){
-
-}
-
-int Player::Dice(){
-
-}
-
-void Human::Move(){
-
+void Player::printCards()
+{
+  cout << "YOUR CARDS" << endl;
+  aTable.printTable();
 }
