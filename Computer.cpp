167a6ffe07fcf19865960c10a8136041ae309ab9
Tanvi Gopalabhatla <tago3904@colorado.edu>
2019-04-17 11:17:19 -0600

    Finished most of the computer.cpp, still need to check other player's cards

--- Computer.cpp
+++ Computer.cpp
@@ -8,32 +8,44 @@ Computer::Computer(){
 
 }
 
-void Computer::RandomTurn(){
+int Computer::ChooseTurn(HashTable h){
   int computerTurn;
   cout << "Main Menu" << endl;
   cout << "1. Suggest" << endl;
   cout << "2. Final Accusation" << endl;
   cout << "3. Quit Turn" << endl;
+  cout << "4. Quit Game" << endl;
 
   //random function between 1 - 3
-  computerTurn = rand() % 3 + 1;
+  computerTurn = rand() % 4 + 1;
 
   if (computerTurn == 1)
   {
-    RandomSuggest();
+    Suggest();
+    return 1;
   }
   else if (computerTurn == 2)
   {
-    FinalAccusation();
+    FinalAccusation(h);
+    return 2;
   }
   else if (computerTurn == 3)
   {
-    cout << "You have quit your turn" << endl;
+    cout << "The computer has quit its turn" << endl; //Switch turn in int main
+    return 3;
+  }
+  else if (computerTurn == 4)
+  {
+    cout << "The computer has quit the game" << endl;
+    return 4;
   }
 
 }
 
-void suspectHelper(){
+void Computer::printCards(){
+  cTable.printTable();
+}
+string suspectHelper(){
   int randSuspect;
   string computerSuggestSuspect;
   //rand function between 6 and 11
@@ -42,17 +54,15 @@ void suspectHelper(){
   //call search function with that random integer
   Card *temp = cTable.searchItem(randSuspect);
 
-  if (temp == NULL)
-  {
-    suspectHelper();
-  }
-  else if (temp != NULL)
+  if(temp != NULL)
   {
     computerSuggestSuspect = temp->name;
   }
+
+  return computerSuggestSuspect;
 }
 
-void weaponHelper(){
+string weaponHelper(){
   int randWeapon;
   string computerSuggestWeapon;
   //rand function between 12 and 17
@@ -61,17 +71,15 @@ void weaponHelper(){
   //call search function with that random integer
   Card *tempW = cTable.searchItem(randWeapon);
 
-  if (tempW == NULL)
-  {
-    weaponHelper();
-  }
-  else if (tempW != NULL)
+  if (tempW != NULL)
   {
     computerSuggestWeapon = tempW->name;
   }
+
+  return computerSuggestWeapon;
 }
 
-void Computer::RandomSuggest(bool final){
+void Computer::Suggest(int roll){
   //After the dice roll
   string computerRoomChoice;
   string computerSuggestRoom;
@@ -81,6 +89,7 @@ void Computer::RandomSuggest(bool final){
 
   if (roll == 1 || roll == 3 || roll == 5)
   {
+    cout << "The computer will now choose a room to go into" << endl;
     cout << "UMC" << endl;
     cout << "Farrand Field" << endl;
     cout << "CSEL" << endl;
@@ -90,7 +99,7 @@ void Computer::RandomSuggest(bool final){
 
     computerDice = rand() % 6 + 1;
 
-    switch(stoi(computerRoomChoice))
+    switch(stoi(computerDice))
     {
       case 1:
         computerSuggestRoom = "UMC";
@@ -105,7 +114,7 @@ void Computer::RandomSuggest(bool final){
       break;
 
       case 4:
-        computerSuggestRoom = "Kittredge Central";
+        computerSuggestRoom = "Kittredge";
       break;
 
       case 5:
@@ -122,28 +131,36 @@ void Computer::RandomSuggest(bool final){
 
     cout << "Computer's Suggestion:" << endl;
     cout << "Room: " << computerSuggestRoom << endl;
-    suspectHelper();
+    computerSuggestSuspect = suspectHelper();
     cout << "Suspect: " << computerSuggestSuspect << endl;
-    weaponHelper();
+    computerSuggestWeapon = weaponHelper();
     cout << "Weapon: " << computerSuggestWeapon << endl;
   }
   else
   {
-    //switch turn function - main?
+    return;
   }
 }
 
-void Computer::FinalAccusation(){
-  //After dice roll
-  //one choice to get the envelope cards correct
+void Computer::FinalAccusation(HashTable h){
+  bool inEnvelope;
+  //After dice roll, one choice to get the envelope cards correct
   RandomSuggest(); //Basically make a Suggestion
 
-  //search envelope hash table
-
+  //search envelope hash table -->helper function called checkEnvelopeComputer
+  inEnvelope = checkEnvelope(computerSuggestWeapon, computerSuggestSuspect, computerSuggestRoom, h);
   //if all cards are found
-  cout << "Congratulations! You successfully uncovered the details of the murder!" << endl;
+  if (inEnvelope == true)
+  {
+    cout << "Congratulations! You successfully uncovered the details of the murder!" << endl;
+  }
+  else
+  {
+    //if none are found or at least one doesn't match
+    cout << "Sorry! You didn't accurately solve the murder mystery." << endl;
+  }
 
-  //if none are found or at least one doesn't match
-  cout << "Sorry! You didn't accurately solve the murder mystery." << endl;
-
+  cout << "The correct answer is:" << endl;
+  h.printTable();
+  return;
 }
