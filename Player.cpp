9c75be8dc8723eff0d1160582501371a5a070a63
Tanvi Gopalabhatla <tago3904@colorado.edu>
2019-04-16 18:50:42 -0600

    Fixing Bugs

--- Player.cpp
+++ Player.cpp
@@ -8,17 +8,27 @@ using namespace std;
 
 Player::Player()
 {
-
+  //Empty.
 }
 
-void Player::Turn(){
-
+void Player::Turn()
+{
+  cout << "Main Menu" << endl;
+  cout << "1. Roll" << endl;
+  cout << "2. Suggest" << endl;
+  cout << "3. Final Accusation" << endl;
 }
 
-int Player::Dice(){
+int Player::Dice()
+{
+  int roll;
+  roll = rand() % 7;
 
+  cout << "You rolled a " << roll << endl;
 }
 
-void Human::Move(){
-
+void Player::printCards()
+{
+  cout << "YOUR CARDS" << endl;
+  aTable.printTable();
 }
