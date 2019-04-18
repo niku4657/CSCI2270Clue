692cf2c5138487d3958aa1c157f699fd718c488b
namitapasupuleti <49174841+namitapasupuleti@users.noreply.github.com>
2019-04-18 17:18:00 -0600

    added for loops to distrib

--- Game.cpp
+++ Game.cpp
@@ -5,6 +5,23 @@ using namespace std;
 void Game::distributeCards(){
   // distribute Cards from array of cards to individual player hash tables
   // merge arrays into one array
+  cc.mergeArrays();
+  // create 3 for loops
+  for (int i = 0; i < 15; i + 3){
+    Card* card = new Card;
+    card = cc.allCards[i];
+    players[0].aTable.insertItem(card->key, card->name);
+  }
+  for (int i = 1; i < 15; i + 3){
+    Card* nextCard = new Card;
+    card = cc.allCards[i];
+    players[1].aTable.insertItem(card->key, card->name);
+  }
+  for (int i = 2; i < 15; i + 3){
+    Card* thirdCard = new Card;
+    card = cc.allCards[i];
+    player[2].aTable.insertItem(card->key, card->name);
+  }
   //Use cc.allCards[] and distribute into players' hashtables using players[]!!
 }
 
